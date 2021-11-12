1e04bd8dafa80461ab566fd3c1fd9392d8f03291
daiki_ito <ito@gizumo-inc.jp>
2021-11-12 10:26:09 +0900

    fix: app,webのファイル構成

--- pipelines/app/output_diagram.sh
+++ pipelines/app/output_diagram.sh
@@ -3,12 +3,13 @@
 mountdir=/var/www/workdir
 workingdir=$mountdir/templates
 dockerimage=cfn-diagram:latest
+dirname=$(echo $1 | tr -d .yml)
 
 docker run \
     -it \
     --rm \
     -v $PWD:$mountdir/ \
     -w $workingdir \
-    $dockerimage cfn-dia html -t $workingdir/diagram.yml -o $mountdir/outputs
+    $dockerimage cfn-dia html -t $workingdir/$1 -o $mountdir/outputs/$dirname
 
 open $PWD/outputs/index.html
