b3fb6e28ef59222b1824defe4ac337ea0a2b8bd3
Ito_Daiki <ito@gizumo-inc.jp>
2021-11-26 14:55:49 +0900

    chore: pipelinesディレクトリ下の構造変更にあたって、ファイル移動・修正

---
+++ pipelines/output_diagram.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mountdir=/var/www/workdir
+workingdir=$mountdir/templates
+dockerimage=cfn-diagram:latest
+dirname=$(echo $1 | tr -d .yml)
+
+docker run \
+    -it \
+    --rm \
+    -v $PWD:$mountdir/ \
+    -w $workingdir \
+    $dockerimage cfn-dia html -t $workingdir/$1 -o $mountdir/outputs/$dirname
+
+open $PWD/outputs/$dirname/index.html
