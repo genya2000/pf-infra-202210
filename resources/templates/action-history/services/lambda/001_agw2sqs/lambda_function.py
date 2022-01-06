f71e75c5f16ecb7230fbefe0b4ac1687151c262e
Sakata_Kiyoshi <sakata@gizumo-inc.jp>
2022-01-06 11:50:51 +0900

    chore: APIGatewayからSQSへのLambdaスクリプトの入れ込み/SQSからLambda経由でDynamoDBへの保存スクリプトの追加

--- resources/templates/action-history/services/lambda/001_agw2sqs/lambda_function.py
+++ resources/templates/action-history/services/lambda/001_agw2sqs/lambda_function.py
@@ -1,8 +1,17 @@
 import json
+import os
+import boto3
+
+#name = 'imedia-sample-lead-action-log'
+name = os.environ['queueName']
+sqs = boto3.resource('sqs')
 
 def lambda_handler(event, context):
-    # TODO implement
+    queue = sqs.get_queue_by_name(QueueName=name)
+    # メッセージ×3をキューに送信
+    response = queue.send_message(MessageBody=json.dumps(event))
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response)
     }
